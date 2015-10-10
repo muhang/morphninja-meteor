1dfe3522371f771a2f8556a0c7b8182be29731f3
millisecond <casey@goroost.com>
2015-10-10 15:01:02 -0400

    Back off polling

--- server/hackernews.js
+++ server/hackernews.js
@@ -1,7 +1,7 @@
 //poll hacker news
 Meteor.startup(function () {
     // code to run on server at startup
-    setInterval(Meteor.bindEnvironment(pollHackerNews), 5000);
+    setInterval(Meteor.bindEnvironment(pollHackerNews), 60000);
 });
 
 function pollHackerNews() {
